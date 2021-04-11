0fdb6d65b99ce298d651d4fc5b1a357a34152c34
powengineer <70952009+powengineer@users.noreply.github.com>
2021-04-11 23:02:21 +0300

    Add files via upload

---
+++ Homeworks/hw2.py
@@ -0,0 +1,10 @@
+userNameRecorded = "A12xw5ht"
+PasswordRecorded = "GSz27pv@1r"
+
+userName = input("Please enter your username:")
+password = input("Please enter your password:")
+
+if userName == userNameRecorded and password == PasswordRecorded:
+    print("Successful login!")
+else:
+    print("Invalid login")
